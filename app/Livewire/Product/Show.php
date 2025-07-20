60e69ce647f166622d6caa92e615903ee55c8f55
Jordy Thijs <github@insyht.nl>
2025-07-20 22:14:32 +0200

    Typo fix

--- app/Livewire/Product/Show.php
+++ app/Livewire/Product/Show.php
@@ -11,7 +11,7 @@ class Show extends Component
 
     public function mount($product)
     {
-        // Might be a product for which then user has no url set.
+        // Might be a product for which the user has no url set.
         // In that case, set $this->product to null and show a 'no urls found' notification
         $this->product = Product::mine()->where('id', $product)->first() ?? null;
     }
